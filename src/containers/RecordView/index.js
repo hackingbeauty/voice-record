42c571e6e157346c93c3214b98da1f45b1ae3870
Mark Muskardin <mark.muskardin@gmail.com>
2017-01-21 21:00:59 -0800

    added react-simple-timer

--- src/containers/RecordView/index.js
+++ src/containers/RecordView/index.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { connect }          from 'react-redux';
 import MicrophoneControls   from 'components/MicrophoneControls';
+import ReactSimpleTimer     from 'react-simple-timer';
 
 import { styles } from './styles.scss';
 
 class RecordView extends Component {
   constructor(props){
     super(props);
+    this.whatever = this.whatever.bind(this);
+    this.state = {
+      startTimer: false
+    }
+  }
+
+  whatever() {
+    this.setState({
+      startTimer: true
+    });
   }
 
   render() {
     return (
       <div className={styles}>
-      <div></div>
         <div id="microphone-section">
           <MicrophoneControls />
         </div>
+        <ReactSimpleTimer play={this.state.startTimer} />
+        <button onTouchTap={this.whatever}>Start</button>
       </div>
     );
   }
